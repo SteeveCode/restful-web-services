890e8c798ef8a2e4fae8d1e33e235234022f75a9
steevecode <stevecode@yahoo.com>
2023-07-11 17:48:00 +0100

    Step 10 - Enhancing POST Method to return correct HTTP Status Code and Location URI

--- src/main/java/com/brexson/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/brexson/restfulwebservices/user/UserDaoService.java
@@ -30,8 +30,9 @@ public class UserDaoService {
         return users.stream().filter(predicate).findFirst().get();
     }
 
-    public void save(User user) {
+    public User save(User user) {
         user.setId(++usersCount);
         users.add(user);
+        return  user;
     }
 }
