84b8896272cc5e86fc96ee45c7771ada4c52103d
steevecode <stevecode@yahoo.com>
2023-07-12 11:53:19 +0100

    Step 12 - Implementing Generic Exception Handling for all Resources

---
+++ src/main/java/com/brexson/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.brexson.restfulwebservices.exception;
+
+import com.brexson.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+//    customized exception message to handle ALL Exceptions i.e. 500 internal server error message is returned
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
+//     customized exception message to handle only NOT_FOUND Exception i.e 500 internal server error message is returned
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+
+    }
+
+
+}
